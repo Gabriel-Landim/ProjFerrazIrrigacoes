4e7ddadec9a7db6c07b221bd61f623986725ba4d
VANESSA DA SILVA <vanessa.silva76@senacsp.edu.br>
2024-04-29 20:14:41 -0300

    alteracaoVanessa

---
+++ Modelo/modCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class modCliente
+    {
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+    }
+}
