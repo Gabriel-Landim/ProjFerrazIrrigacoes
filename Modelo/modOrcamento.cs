b80e6ea21a1bf07d27cbf9cd23805edbc48d7254
gabriel.olandim <gabriel.olandim@PPR0561023W10-1>
2024-04-29 21:19:12 -0300

    Conclusão Gabriel

---
+++ Modelo/modOrcamento.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class modOrcamento
+{
+	public int Id { get; set; }
+	public decimal Valor { get; set; }
+    public DateTime DataOrcamento { get; set; }
+    public int ClienteId { get; set; }
+    public int VendaId { get; set; }
+
