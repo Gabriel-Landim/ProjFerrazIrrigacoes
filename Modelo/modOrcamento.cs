7d430e4d0476918d540008ca7831ea423324bf7d
gabriel.olandim <gabriellandim12@hotmail.com>
2024-04-29 21:44:02 -0300

    Alteração concluida 6

--- Modelo/modOrcamento.cs
+++ Modelo/modOrcamento.cs
@@ -2,9 +2,11 @@
 
 public class modOrcamento
 {
-	public int Id { get; set; }
-	public decimal Valor { get; set; }
+    public int Id { get; set; }
+    public decimal Valor { get; set; }
     public DateTime DataOrcamento { get; set; }
     public int ClienteId { get; set; }
     public int VendaId { get; set; }
+
+}
 
