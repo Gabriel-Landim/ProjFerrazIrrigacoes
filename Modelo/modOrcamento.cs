d720270bbeabaae64a250127ade451b022a813e0
hugorferraz <fhugoferraz@gmail.com>
2024-05-07 20:15:13 -0300

    modOrcamento

--- Modelo/modOrcamento.cs
+++ Modelo/modOrcamento.cs
@@ -2,5 +2,9 @@
 
 public class modOrcamento
 {
-	public int Id { get; set; }
+    public int Id { get; set; }
+    public double Valor { get; set; }
+    public DateTime DataOrcamento { get; set; }
+    public int IdCliente { get; set; }
+    public int IdOrcamento { get; set; }
 }
