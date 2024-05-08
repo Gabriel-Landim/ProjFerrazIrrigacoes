86f5e68640f34bdeefa7c603b2c5e070dea1744d
hugorferraz <fhugoferraz@gmail.com>
2024-05-07 21:19:09 -0300

    correção dalFormaDePagamento

--- DAL/dalFormaDePagamento.cs
+++ DAL/dalFormaDePagamento.cs
@@ -119,7 +119,7 @@ namespace DAL
                 //Passsa os valores para o comando SQL pelos parametros @login e @senha
                 cmd.Parameters.AddWithValue("@ID", objDados.Id);
                 cmd.Parameters.AddWithValue("@DESCRICAO", objDados.Descricao);
-                
+
 
                 cmd.Connection = cn;
                 cn.Open();
