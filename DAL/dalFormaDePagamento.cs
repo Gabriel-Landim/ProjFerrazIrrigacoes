045e608d61b83945bede3a9b68f17fea9353296f
hugorferraz <fhugoferraz@gmail.com>
2024-05-07 21:45:22 -0300

    Alteracao Sergio ItensOrcamento

--- DAL/dalFormaDePagamento.cs
+++ DAL/dalFormaDePagamento.cs
@@ -10,63 +10,6 @@ namespace DAL
 {
     public class dalFormaDePagamento
     {
-        public List<modFormaDePagamento> CarregarFormaDePagamento()
-        {
-            //Variavel de Conexao
-            SqlConnection cn = new SqlConnection();
-            try
-            {
-                cn.ConnectionString = Dados.StringDeConexao;
-                //Variavel do comando
-                SqlCommand cmd = new SqlCommand();
-                cmd.CommandText = " SELECT ID, DESCRICAO FROM FORMADEPAGAMENTO" +
-                                  " ORDER BY ID ";
-
-                //Passsa os valores para o comando SQL pelos parametros @login e @senha
-
-                cmd.Connection = cn;
-                cn.Open();
-
-                //Executando o comando e armazenando o resultado em registro
-                SqlDataReader registro = cmd.ExecuteReader();
-                cmd.Dispose();
-
-                //Criar uma lista para armazenar os dados.
-                var ListaFormaDePag = new List<modFormaDePagamento>();
-
-
-                if (registro.HasRows)
-                {
-                    while (registro.Read())
-                    {
-                        ListaFormaDePag.Add(new modFormaDePagamento()
-                        {
-                            Id = Convert.ToInt32(registro["Id"]),
-                            Descricao = Convert.ToString(registro["Descricao"]),
-
-                        });
-                    }
-                }
-
-
-                return ListaFormaDePag;
-
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception("Erro SQL: " + ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Erro SQL: " + ex.Message);
-            }
-            finally
-            {
-                cn.Close();
-                cn.Dispose();
-            }
-
-        }
         public void Insere(modFormaDePagamento objDados)
         {
             //Variavel de Conexao
@@ -76,11 +19,11 @@ namespace DAL
                 cn.ConnectionString = Dados.StringDeConexao;
                 //Variavel do comando
                 SqlCommand cmd = new SqlCommand();  //objeto de comando
-                cmd.CommandText = " INSERT INTO DESCRICAO FROM FORMADEPAGAMENTO" +  //comando que eu quero
-                                  " VALUES (@DESCRICAO) ";
+                cmd.CommandText = " INSERT INTO ID,NOME, CPF, TELEFONE, EMAIL, SENHA, RUA, BAIRRO, CEP, NUMERO, COMPLEMENTO, CIDADE FROM CLIENTE" +  //comando que eu quero
+                                  " VALUES (@CLIENTE) ";
 
                 //Passsa os valores para o comando SQL pelos parametros @login e @senha
-                cmd.Parameters.AddWithValue("@DESCRICAO", objDados.Descricao);
+                cmd.Parameters.AddWithValue("@CLIENTE", objDados.Descricao);
                 cmd.Connection = cn;
                 cn.Open();
 
@@ -119,7 +62,7 @@ namespace DAL
                 //Passsa os valores para o comando SQL pelos parametros @login e @senha
                 cmd.Parameters.AddWithValue("@ID", objDados.Id);
                 cmd.Parameters.AddWithValue("@DESCRICAO", objDados.Descricao);
-                
+
 
                 cmd.Connection = cn;
                 cn.Open();
