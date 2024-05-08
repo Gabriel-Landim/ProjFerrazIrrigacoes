49b13ac429dc172021adbd2e3ba1a88d0e31fa4a
hugorferraz <fhugoferraz@gmail.com>
2024-05-07 22:26:52 -0300

    Alteração dalOrcamento

--- DAL/dalOrcamento.cs
+++ DAL/dalOrcamento.cs
@@ -1,12 +1,185 @@
-﻿using System;
+﻿using Modelo;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class dalOrcamento
+    public class dalMarca
     {
+        public List<modOrcamento> CarregarOrcamento()
+        {
+            //Variavel de Conexao
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Dados.StringDeConexao;
+                //Variavel do comando
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = " SELECT ID, NOME FROM MARCA" +
+                                  " ORDER BY NOME ";
+
+                //Passsa os valores para o comando SQL pelos parametros @login e @senha
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                //Executando o comando e armazenando o resultado em registro
+                SqlDataReader registro = cmd.ExecuteReader();
+                cmd.Dispose();
+
+                //Criar uma lista para armazenar os dados.
+                var ListaMarca = new List<modMarca>();
+
+
+                if (registro.HasRows)
+                {
+                    while (registro.Read())
+                    {
+                        ListaMarca.Add(new modMarca()
+                        {
+                            Id = Convert.ToInt32(registro["Id"]),
+                            NomeMarca = Convert.ToString(registro["Nome"]),
+                        });
+                    }
+                }
+
+
+                return ListaMarca;
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro SQL: " + ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro SQL: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+
+        }
+        public void Insere(modMarca objDados)
+        {
+            //Variavel de Conexao
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Dados.StringDeConexao;
+                //Variavel do comando
+                SqlCommand cmd = new SqlCommand();  //objeto de comando
+                cmd.CommandText = " INSERT INTO NOME FROM MARCA" +  //comando que eu quero
+                                  " VALUES (@NOME) ";
+
+                //Passsa os valores para o comando SQL pelos parametros @login e @senha
+                cmd.Parameters.AddWithValue("@NOME", objDados.NomeMarca);
+                cmd.Connection = cn;
+                cn.Open();
+
+                //Executando o comando e armazenando o resultado em registro
+                cmd.ExecuteNonQuery();  //execução do comando
+                cmd.Dispose();
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro SQL: " + ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro SQL: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+
+        }
+        public void Alterar(modMarca objDados)
+        {
+            //Variavel de Conexao
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Dados.StringDeConexao; //onde disparar o comando
+                //Variavel do comando
+                SqlCommand cmd = new SqlCommand();  //objeto de comando
+                cmd.CommandText = " UPDATE MARCA SET NOME = @NOME " +  //comando que eu quero
+                                  " WHERE ID = @ID ";
+
+                //Passsa os valores para o comando SQL pelos parametros @login e @senha
+                cmd.Parameters.AddWithValue("@ID", objDados.Id);
+                cmd.Parameters.AddWithValue("@NOME", objDados.NomeMarca);
+
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                //Executando o comando e armazenando o resultado em registro
+                cmd.ExecuteNonQuery();  //execução do comando
+                cmd.Dispose();
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro SQL: " + ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro SQL: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+
+        }
+        public void Excluir(int id)
+        {
+            //Variavel de Conexao
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = Dados.StringDeConexao; //onde disparar o comando
+                //Variavel do comando
+                SqlCommand cmd = new SqlCommand();  //objeto de comando
+                cmd.CommandText = " DELETE FROM MARCA " +  //comando que eu quero
+                                  " WHERE ID = @ID ";
+
+                //Passsa os valores para o comando SQL pelos parametros @login e @senha
+                cmd.Parameters.AddWithValue("ID", id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                //Executando o comando e armazenando o resultado em registro
+                cmd.ExecuteNonQuery();  //execução do comando
+                cmd.Dispose();
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Erro SQL: " + ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro SQL: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+
+        }
     }
 }
