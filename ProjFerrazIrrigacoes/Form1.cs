02a0052b97d9ff3f722970e28da326609b46d2a0
gabriel.olandim <gabriel@landim.dev>
2024-05-09 20:29:41 -0300

    Criação Form principal

--- ProjFerrazIrrigacoes/Form1.cs
+++ ProjFerrazIrrigacoes/Form1.cs
@@ -7,21 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace ProjFerrazIrrigacoes
 {
     public partial class Form1 : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr GetProcAddress(
+            int nLeftRect,
+            int nTopRect,
+            int nLeftReact,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
         public Form1()
         {
             InitializeComponent();
+            //Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            int a;
-            a = 1;
-            string tr;
+            //int a;
+            //a = 1;
+            //string tr;
+        }
+
+        private void BtnCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            int Teste;
         }
     }
 }
