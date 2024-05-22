8000d651cb4e019af3ff3965e4c1886cc305ce7c
gabriel.olandim <gabriel@landim.dev>
2024-05-21 21:50:48 -0300

    Finalizado cadastros

--- BLL/bllCliente.cs
+++ BLL/bllCliente.cs
@@ -18,7 +18,7 @@ namespace BLL
 
         public void Inserir(modCliente objdados)
         {
-            if ((objdados.NomeCliente.Trim() != "") && (objdados.Cpf.Trim() != ""))
+            if (objdados.NomeCliente.Trim() != "")
             {
                 dalCliente objInserir = new dalCliente();
                 objInserir.Insere(objdados);
